2d6feece500347554adc4dfff6c9a4007e791e1f
dmkofgl <dmkofgl@gmail.com>
2018-10-15 15:16:44 +0300

    add BrowserWindow

---
+++ src/Stand/Stand/Windows/BrowserWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Stand.UI.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для BrowserWindow.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _currentUrl;
+
+        public string CurrentUrl
+        {
+            get { return _currentUrl; }
+            set
+            {
+                if (_currentUrl != value)
+                {
+                    _currentUrl = value;
+                    OnPropertyChanged("CurrentUrl");
+                }
+            }
+        }
+        public BrowserWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            var inputKey = (sender as TextBox).Text;
+            if (inputKey != null)
+            {
+                LoadWebPage(inputKey);
+            }
+        }
+        private void LoadWebPage(string url)
+        {
+            Browser.Navigate(url);
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void OnSubmit_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_currentUrl!=null)
+            {
+                Browser.Navigate(_currentUrl);
+            }
+          
+        }
+    }
+}
